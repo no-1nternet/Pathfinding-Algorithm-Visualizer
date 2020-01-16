27d1085af057b84fbaff82c0df8697fb393749df
Steven <stevenyan53@gmail.coim>
2020-01-16 21:53:49 +1300

    a basic version with dijkstra and a little animation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Pathfinder from "./Pathfinder/Pathfinder";
+function App() {
+  return (
+    <div className="App">
+      <Pathfinder />
+    </div>
+  );
+}
+
+export default App;
